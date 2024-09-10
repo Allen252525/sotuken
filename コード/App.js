3bb141212400b7eaeae1521e0d6ae5737701daec
tomitani21 <22110044@oic-ok.ac.jp>
2024-09-10 15:26:22 +0900

    ２つ手札が出るように改変

--- コード/App.js
+++ コード/App.js
@@ -8,23 +8,56 @@ let totalSpades = 0;
 //デッキAPIのURL
 const deckApiUrl = "https://deckofcardsapi.com/api/deck";
 
-// ゲーム開始時にデッキを取得
+
+
+
+
+// ゲーム開始時にデッキを取得(エネミー)
 window.onload = () => {
+    fetch(`${deckApiUrl}/new/shuffle/?cards=5S,6S,7S,8S,9S,10S,JS,QS,KS,AS,5D,6D,7D,8D,9D,10D,JD,QD,KD,AD,5C,6C,7C,8C,9C,10C,JC,QC,KC,AC,5H,6H,7H,8H,9H,10H,JH,QH,KH,AH
+`)
+        .then(response => response.json())
+        .then(data => {
+            EnemyDeckId = data.deck_id;
+        });
+
+
+
+// ゲーム開始時にデッキを取得(プレイヤーデッキ)
+
     fetch(`${deckApiUrl}/new/shuffle/?cards=2S,3S,4S,2D,3D,4D,2C,3C,4C,2H,3H,4H,X1,X2&jokers_enabled=true`)
         .then(response => response.json())
         .then(data => {
             deckId = data.deck_id;
         });
-};
-// ゲーム開始時にデッキを取得(エネミー)
-window.onload = () => {
-    fetch(`${deckApiUrl}/new/shuffle/?cards=5S,6S,7S,8S,9S,10S,JS,QS,KS,AS,5D,6D,7D,8D,9D,10D,JD,QD,KD,AD,5C,6C,7C,8C,9C,10C,JC,QC,KC,AC,5H,6H,7H,8H,9H,10H,JH,QH,KH,AH
-&jokers_enabled=true`)
+
+
+
+enemy手札
+function EnemyDrawCards() {
+    fetch(`${deckApiUrl}/${EnemyDeckId}/draw/?count=4`)
         .then(response => response.json())
-        .then(data => {
-            EnemyDeckId = data.deck_id;
+        .then(data1 => {
+            const drawnCardsDiv = document.getElementById('enemy-drawn-cards');
+            drawnCardsDiv.innerHTML = ''; // 以前のカードをクリア
+
+            data1.cards.forEach(card => {
+                const cardImg = document.createElement('img');
+                cardImg.src = card.image;
+                cardImg.alt = `${card.value} of ${card.suit}`;
+                cardImg.dataset.value = card.value;
+                cardImg.dataset.suit = card.suit;
+
+                cardImg.onclick = () => selectCard(cardImg);
+
+                drawnCardsDiv.appendChild(cardImg);
+            });
         });
+}
+
+
 };
+
 
 // 山札からカードを引く
 function drawCards() {
@@ -48,27 +81,10 @@ function drawCards() {
         });
 }
 
-//enemy手札
-function EnemyDrawCards() {
-    fetch(`${deckApiUrl}/${EnemyDeckId}/draw/?count=4`)
-        .then(response => response.json())
-        .then(data => {
-            const drawnCardsDiv = document.getElementById('enemy-drawn-cards');
-            drawnCardsDiv.innerHTML = ''; // 以前のカードをクリア
 
-            data.cards.forEach(card => {
-                const cardImg = document.createElement('img');
-                cardImg.src = card.image;
-                cardImg.alt = `${card.value} of ${card.suit}`;
-                cardImg.dataset.value = card.value;
-                cardImg.dataset.suit = card.suit;
 
-                cardImg.onclick = () => selectCard(cardImg);
 
-                drawnCardsDiv.appendChild(cardImg);
-            });
-        });
-}
+
 
 // カードを選択してリストに追加、場から削除
 // function selectCard(cardElement) {
